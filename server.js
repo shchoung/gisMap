9c047441b4f5f262b27d120b9f9a66c632533bc9
shchoung <raphael31004@naver.com>
2026-06-02 11:46:50 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const path = require('path');
+const path    = require('path');
+const fs      = require('fs');
 
-const app = express();
+const app  = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  let html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf-8');
+  html = html.replace('__CESIUM_TOKEN__', process.env.CESIUM_TOKEN || '');
+  res.send(html);
 });
 
-app.listen(PORT, () => {
-  console.log(`Disaster Sim running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
